fda473f1abc77169b20cf225ae7cc4f7f12ff461
ping <watergolp@163.com>
2018-09-05 18:00:52 +0800

    config mysql

--- new_project/settings.py
+++ new_project/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'new_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+
+
+    #使用mysql为默认数据库
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT0': '3306',
+        'NAME': 'guest',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    },
 }
 
 
